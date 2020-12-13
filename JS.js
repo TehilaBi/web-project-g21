5e7665fc394b1bbb99bde2d32b0acef19b61f934
Tehila Biton <biton187@gmail.com>
2020-12-14 01:12:40 +0200

    Update Gift Card page and JS
    add validation function to check input in Gift Card page

--- JS.js
+++ JS.js
@@ -1,8 +1,19 @@
 
+function validateGiftInput(amount, phoneNum) {
+    var ValidAmount;
 
-function MessBox(){
-    alert("Add to cart successfully!");
-    location.href='../html/general.html';
+    // If amount is Not a Number or less than 0
+    if (isNaN(amount) || amount < 0) {
+        ValidAmount=false;
+        alert("Please enter amount greater than 0");
+    } else {
+        ValidAmount=true;
+    }
 
-}
+    if(ValidAmount==true){
+        alert("Add to cart successfully!");
+    }
+
+    
+  }
 
