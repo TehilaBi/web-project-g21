e64d314bcb6b0cd9d5ee4905de8cc2fed6e97c07
Chloe1511 <60323204+Chloe1511@users.noreply.github.com>
2020-12-14 19:09:39 +0200

    Update JS.js

--- JS.js
+++ JS.js
@@ -21,6 +21,7 @@ function validateGiftInput(amount,email) {
 
     if(ValidAmount==true && ValidEmail==true){
         alert("Add to cart successfully!");
+        window.open('../html/general.html');
     }
 
     
@@ -29,12 +30,12 @@ function validateGiftInput(amount,email) {
 
 function MessBox_contactMe(){
     alert("Thank you! Your message has been received");
-    location.href='../html/general.html';
+    window.open('../html/general.html');
 
 }
 
 function MessBox_connect(){
     alert("Connect successfully!");
-    location.href='../html/general.html';
+    window.open('../html/general.html');
 
 }
