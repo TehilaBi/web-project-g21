b1a6713bf52fa9b338a3026d642e9db5d447fa3d
TomerLan <tomerlan213@gmail.com>
2020-12-14 12:15:03 +0200

    Update contact+connect pages, add css for contact+connect, add functions to JS

--- JS.js
+++ JS.js
@@ -6,3 +6,15 @@ function MessBox(){
 
 }
 
+
+function MessBox_contactMe(){
+    alert("Thank you! Your message has been received");
+    location.href='../html/general.html';
+
+}
+
+function MessBox_connect(){
+    alert("Connect successfully!");
+    location.href='../html/general.html';
+
+}
