c10d9399f7c677bd1dc9a21148a97b878de37c2f
Tehila Biton <biton187@gmail.com>
2020-12-16 01:24:37 +0200

    update recipes and shop
    update filler option in shop page
    update text recipes

--- JS.js
+++ JS.js
@@ -53,3 +53,51 @@ function addedToCart(){
     window.open('../html/shop.html');
     window.close();
 }
+
+
+function filterSelection(c) {
+  var x, j;
+  x = document.getElementsByClassName("filterDiv");
+  if (c == "all") c = "";
+  // Add the "show" class (display:block) to the filtered elements, and remove the "show" class from the elements that are not selected
+  for (j = 0; j < x.length; j++) {
+    RemoveClass(x[j], "show");
+    if (x[j].className.indexOf(c) > -1) AddClass(x[j], "show");
+  }
+}
+
+// Show filtered elements
+function AddClass(element, name) {
+  var j, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (j = 0; j < arr2.length; j ++) {
+    if (arr1.indexOf(arr2[j]) == -1) {
+      element.className += " " + arr2[j];
+    }
+  }
+}
+
+// Hide elements that are not selected
+function RemoveClass(element, name) {
+  var j, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (j = 0; j < arr2.length; j++) {
+    while (arr1.indexOf(arr2[j]) > -1) {
+      arr1.splice(arr1.indexOf(arr2[j]), 1);
+    }
+  }
+  element.className = arr1.join(" ");
+}
+
+// Add active class to the current control button (highlight it)
+var btnContainer = document.getElementById("myBtnContainer");
+var btns = btnContainer.getElementsByClassName("btn");
+for (var j = 0; j < btns.length; j++) {
+  btns[j].addEventListener("click", function() {
+    var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+  });
+}
